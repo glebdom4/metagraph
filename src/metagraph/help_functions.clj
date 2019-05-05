93710485d0207aa683b4ed1bd51705b21c599e28
glebdom4 <glebdom4@gmail.com>
2019-05-05 05:37:32 +0300

    Added current date & time function

--- src/metagraph/help_functions.clj
+++ src/metagraph/help_functions.clj
@@ -1,5 +1,9 @@
 (ns metagraph.help-functions
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [java-time :as jt]))
 
 (defn get-class-name [obj]
   (second (str/split (str (type obj)) #" ")))
+
+(defn now []
+  (str (jt/local-date) "T" (jt/format "hh:mm:ss" (jt/local-time))))
