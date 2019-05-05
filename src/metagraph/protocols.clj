6a9f1e4a4a50b15eaf7c46b49d565774a647f853
glebdom4 <glebdom4@gmail.com>
2019-05-05 13:22:53 +0300

    Fixed a typo in the comments

--- src/metagraph/protocols.clj
+++ src/metagraph/protocols.clj
@@ -9,5 +9,5 @@
   "A protocol for operations with metagraph components"
   (contains-vertex? [this vertex] "Checks if the component contains a vertex")
   (add-to-fragment [this fragment mv-name] "Adds the component to a fragment")
-  (remove-from-fragment [this fragment] "Removes the component to a fragment")
-  (t-remove-from-fragment [this fragment] "Transitively removes the component to a fragment"))
+  (remove-from-fragment [this fragment] "Removes the component from a fragment")
+  (t-remove-from-fragment [this fragment] "Transitively removes the component from a fragment"))
